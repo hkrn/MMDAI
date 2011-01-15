0401230e28622a3a47ff1b9cab64c7b29101b257
hkrn <hikarin.jp@gmail.com>
2011-01-15 23:40:06 +0900

    forgot to add required headers

---
+++ libMMDFiles/src/AlignedMotionState.h
@@ -0,0 +1,62 @@
+/* ----------------------------------------------------------------- */
+/*                                                                   */
+/*  Copyright (c) 2009-2010  Nagoya Institute of Technology          */
+/*                           Department of Computer Science          */
+/*                2010-2011  hkrn (libMMDAI)                         */
+/*                                                                   */
+/* All rights reserved.                                              */
+/*                                                                   */
+/* Redistribution and use in source and binary forms, with or        */
+/* without modification, are permitted provided that the following   */
+/* conditions are met:                                               */
+/*                                                                   */
+/* - Redistributions of source code must retain the above copyright  */
+/*   notice, this list of conditions and the following disclaimer.   */
+/* - Redistributions in binary form must reproduce the above         */
+/*   copyright notice, this list of conditions and the following     */
+/*   disclaimer in the documentation and/or other materials provided */
+/*   with the distribution.                                          */
+/* - Neither the name of the MMDAgent project team nor the names of  */
+/*   its contributors may be used to endorse or promote products     */
+/*   derived from this software without specific prior written       */
+/*   permission.                                                     */
+/*                                                                   */
+/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND            */
+/* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,       */
+/* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF          */
+/* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE          */
+/* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS */
+/* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,          */
+/* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED   */
+/* TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,     */
+/* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON */
+/* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,   */
+/* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY    */
+/* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           */
+/* POSSIBILITY OF SUCH DAMAGE.                                       */
+/* ----------------------------------------------------------------- */
+
+/* AlignedMotionState: MotionState for bullet physics, to align rigid body to the position of corresponding bone for rigid body type 2 */
+class AlignedMotionState : public btMotionState
+{
+private:
+
+   PMDBone *m_bone;
+   btTransform m_boneTrans;
+   btTransform m_boneTransInv;
+   btTransform m_graphicsWorldTrans;
+
+public:
+
+   /* AlignedMotionState: constructor */
+   AlignedMotionState(const btTransform &startTrans, const btTransform &boneTrans, PMDBone *bone);
+
+   /* ~AlignedMotionState: destructor */
+   virtual ~AlignedMotionState();
+
+   /* getWorldTransform: get world transform */
+   virtual void getWorldTransform(btTransform &worldTrans) const;
+
+   /* setWorldTransform: set world transform */
+   virtual void setWorldTransform(const btTransform &worldTrans);
+};
