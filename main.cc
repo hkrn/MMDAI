14bc24c57a84ad252a7ca2bf14122731cd37db4e
hkrn <hikarin.jp@gmail.com>
2011-02-13 23:24:03 +0900

    should use Q_IMPORT_PLUGIN if QMA_BUNDLE_PLUGINS defines

--- main.cc
+++ main.cc
@@ -37,11 +37,13 @@
 #include <QtGui/QApplication>
 #include "QMAWindow.h"
 
+#ifdef QMA_BUNDLE_PLUGINS
 Q_IMPORT_PLUGIN(qma_audio_plugin);
 Q_IMPORT_PLUGIN(qma_julius_plugin);
 Q_IMPORT_PLUGIN(qma_lookat_plugin);
 Q_IMPORT_PLUGIN(qma_openjtalk_plugin);
 Q_IMPORT_PLUGIN(qma_vimanager_plugin);
+#endif
 
 int main(int argc, char *argv[])
 {
