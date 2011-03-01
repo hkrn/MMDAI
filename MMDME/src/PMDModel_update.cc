0e08e6fc6434334b2e8fe3c5f42b426ae0a99944
hkrn <hikarin.jp@gmail.com>
2011-03-02 01:26:20 +0900

    added const modifier as possible (MMDME)

--- MMDME/src/PMDModel_update.cc
+++ MMDME/src/PMDModel_update.cc
@@ -70,20 +70,16 @@ void PMDModel::updateBone()
 /* PMDModel::updateBoneFromSimulation: update bone transform from rigid body */
 void PMDModel::updateBoneFromSimulation()
 {
-   unsigned long i;
-
-   for (i = 0; i < m_numRigidBody; i++)
+   for (unsigned int i = 0; i < m_numRigidBody; i++)
       m_rigidBodyList[i].applyTransformToBone();
 }
 
 /* PMDModel::updateFace: update face morph from current face weights */
 void PMDModel::updateFace()
 {
-   unsigned short i;
-
    if (m_faceList) {
       m_baseFace->apply(m_vertexList);
-      for (i = 0; i < m_numFace; i++)
+      for (unsigned short i = 0; i < m_numFace; i++)
          if (m_faceList[i].getWeight() > kMinFaceWeight)
             m_faceList[i].add(m_vertexList, m_faceList[i].getWeight());
    }
@@ -92,16 +88,12 @@ void PMDModel::updateFace()
 /* PMDModel::updateSkin: update skin data from bone orientation */
 void PMDModel::updateSkin()
 {
-   unsigned short i;
-   unsigned long j;
-   btVector3 v, v2, n, n2;
-
    /* calculate transform matrix for skinning (global -> local) */
-   for (i = 0; i < m_numBone; i++)
+   for (unsigned short i = 0; i < m_numBone; i++)
       m_boneList[i].calcSkinningTrans(&(m_boneSkinningTrans[i]));
 
    /* do skinning */
-   for (j = 0; j < m_numVertex; j++) {
+   for (unsigned int j = 0; j < m_numVertex; j++) {
       if (m_boneWeight1[j] >= 1.0f - kMinBoneWeight) {
          /* bone 1 */
          m_skinnedVertexList[j] = m_boneSkinningTrans[m_bone1List[j]] * m_vertexList[j];
@@ -112,10 +104,10 @@ void PMDModel::updateSkin()
          m_skinnedNormalList[j] = m_boneSkinningTrans[m_bone2List[j]].getBasis() * m_normalList[j];
       } else {
          /* lerp */
-         v = m_boneSkinningTrans[m_bone1List[j]] * m_vertexList[j];
-         n = m_boneSkinningTrans[m_bone1List[j]].getBasis() * m_normalList[j];
-         v2 = m_boneSkinningTrans[m_bone2List[j]] * m_vertexList[j];
-         n2 = m_boneSkinningTrans[m_bone2List[j]].getBasis() * m_normalList[j];
+         const btVector3 v = m_boneSkinningTrans[m_bone1List[j]] * m_vertexList[j];
+         const btVector3 n = m_boneSkinningTrans[m_bone1List[j]].getBasis() * m_normalList[j];
+         const btVector3 v2 = m_boneSkinningTrans[m_bone2List[j]] * m_vertexList[j];
+         const btVector3 n2 = m_boneSkinningTrans[m_bone2List[j]].getBasis() * m_normalList[j];
          m_skinnedVertexList[j] = v2.lerp(v, m_boneWeight1[j]);
          m_skinnedNormalList[j] = n2.lerp(n, m_boneWeight1[j]);
       }
@@ -125,16 +117,14 @@ void PMDModel::updateSkin()
 /* PMDModel::updateToon: update toon coordinates and edge vertices */
 void PMDModel::updateToon(btVector3 *light)
 {
-   unsigned long i;
-
    if (m_toon) {
       /* calculate toon texture coordinates for toon shading */
-      for (i = 0; i < m_numVertex; i++) {
+      for (unsigned int i = 0; i < m_numVertex; i++) {
          m_toonTexCoordList[i].u = 0.0f;
          m_toonTexCoordList[i].v = (1.0f - light->dot(m_skinnedNormalList[i])) * 0.5f;
       }
       /* calculate vertices for edge drawing */
-      for (i = 0; i < m_numVertex; i++) {
+      for (unsigned int i = 0; i < m_numVertex; i++) {
          /* not push vertices with no edge flag */
          if (m_noEdgeFlag[i] == 1)
             m_edgeVertexList[i] = m_skinnedVertexList[i];
@@ -147,10 +137,10 @@ void PMDModel::updateToon(btVector3 *light)
 /* PMDModel::updateShadowColorTexCoord: update / create pseudo toon coordinates for shadow rendering pass on shadow mapping */
 void PMDModel::updateShadowColorTexCoord(float coef)
 {
-   unsigned long i;
    bool update = false;
 
-   if (!m_toon) return;
+   if (!m_toon)
+     return;
 
    if (m_toonTexCoordListForShadowMap == NULL) {
       m_toonTexCoordListForShadowMap = static_cast<TexCoord *>(MMDAIMemoryAllocate(sizeof(TexCoord) * m_numVertex));
@@ -160,7 +150,7 @@ void PMDModel::updateShadowColorTexCoord(float coef)
    }
 
    if (update && m_toonTexCoordListForShadowMap != NULL) {
-      for (i = 0 ; i < m_numVertex ; i++) {
+      for (unsigned int i = 0 ; i < m_numVertex ; i++) {
          m_toonTexCoordListForShadowMap[i].u = 0.0f;
          m_toonTexCoordListForShadowMap[i].v = coef;
       }
@@ -171,22 +161,23 @@ void PMDModel::updateShadowColorTexCoord(float coef)
 /* PMDModel::calculateBoundingSphereRange: calculate the bounding sphere for depth texture rendering on shadow mapping */
 float PMDModel::calculateBoundingSphereRange(btVector3 *cpos)
 {
-   unsigned long i;
-   btVector3 centerPos, v;
-   float maxR = 0.0f, r2;
+   float maxR = 0.0f;
+   btVector3 centerPos;
 
    if (m_centerBone) {
       centerPos = m_centerBone->getTransform()->getOrigin();
-      for (i = 0; i < m_numVertex; i += 10) {
-         r2 = centerPos.distance2(m_skinnedVertexList[i]);
-         if (maxR < r2) maxR = r2;
+      for (unsigned int i = 0; i < m_numVertex; i += 10) {
+         const float r2 = centerPos.distance2(m_skinnedVertexList[i]);
+         if (maxR < r2)
+           maxR = r2;
       }
       maxR = sqrtf(maxR) * 1.1f;
    } else {
       maxR = 0.0f;
    }
 
-   if (cpos) *cpos = centerPos;
+   if (cpos)
+     *cpos = centerPos;
 
    return maxR;
 }
@@ -194,21 +185,18 @@ float PMDModel::calculateBoundingSphereRange(btVector3 *cpos)
 /* PMDModel::smearAllBonesToDefault: smear all bone pos/rot into default value (rate 1.0 = keep, rate 0.0 = reset) */
 void PMDModel::smearAllBonesToDefault(float rate)
 {
-   unsigned short i;
    const btVector3 v(0.0f, 0.0f, 0.0f);
    const btQuaternion q(0.0f, 0.0f, 0.0f, 1.0f);
-   btVector3 tmpv;
-   btQuaternion tmpq;
 
-   for (i = 0; i < m_numBone; i++) {
-      tmpv = (*(m_boneList[i].getCurrentPosition()));
-      tmpv = v.lerp(tmpv, rate);
-      m_boneList[i].setCurrentPosition(&tmpv);
-      tmpq = (*(m_boneList[i].getCurrentRotation()));
-      tmpq = q.slerp(tmpq, rate);
-      m_boneList[i].setCurrentRotation(&tmpq);
+   for (unsigned short i = 0; i < m_numBone; i++) {
+      const btVector3 tmpv1 = (*(m_boneList[i].getCurrentPosition()));
+      btVector3 tmpv2 = v.lerp(tmpv1, rate);
+      m_boneList[i].setCurrentPosition(&tmpv2);
+      const btQuaternion tmpq1 = (*(m_boneList[i].getCurrentRotation()));
+      btQuaternion tmpq2 = q.slerp(tmpq1, rate);
+      m_boneList[i].setCurrentRotation(&tmpq2);
    }
-   for (i = 0; i < m_numFace; i++) {
+   for (unsigned short i = 0; i < m_numFace; i++) {
       m_faceList[i].setWeight(m_faceList[i].getWeight() * rate);
    }
 }
