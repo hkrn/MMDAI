bdab0442c69a91701a2b522eaf58a476c02b4b1f
hkrn <hikarin.jp@gmail.com>
2011-04-20 02:03:04 +0900

    v0.61

--- QMA/main.cc
+++ QMA/main.cc
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
     app.setOrganizationDomain("hkrn.github.com");
     app.setOrganizationName("MMDAI Project");
     app.setApplicationName("QtMMDAI");
-    app.setApplicationVersion("0.6");
+    app.setApplicationVersion("0.61");
 
     QDir appDir = QDir(app.applicationDirPath());
 #if defined(Q_OS_WIN)
