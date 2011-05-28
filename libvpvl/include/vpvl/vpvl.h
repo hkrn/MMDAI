224a0ff07b49b4dbfd0aba0dbfd4f5a096bd5b18
hkrn <hikarin.jp@gmail.com>
2011-05-28 17:29:19 +0900

    removed (would be unused) interfaces

--- libvpvl/include/vpvl/vpvl.h
+++ libvpvl/include/vpvl/vpvl.h
@@ -1,5 +1,6 @@
 #ifndef vpvl_vpvl_H_
 #define vpvl_vpvl_H_
+
 #include "vpvl/common.h"
 #include "vpvl/Bone.h"
 #include "vpvl/BoneKeyFrame.h"
@@ -8,12 +9,9 @@
 #include "vpvl/Face.h"
 #include "vpvl/FaceKeyFrame.h"
 #include "vpvl/IK.h"
-#include "vpvl/IModel.h"
-#include "vpvl/IModelParser.h"
-#include "vpvl/IMotion.h"
-#include "vpvl/IMotionParser.h"
 #include "vpvl/Material.h"
 #include "vpvl/RigidBody.h"
 #include "vpvl/Vertex.h"
+
 #endif /* vpvl_vpvl_H_ */
 
