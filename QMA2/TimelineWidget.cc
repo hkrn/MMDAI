152c0a2f517be42962c74b17998b1225ab30ff6d
hkrn <hikarin.jp@gmail.com>
2011-07-03 00:58:51 +0900

    effective setModel and added painting (but not used)

--- QMA2/TimelineWidget.cc
+++ QMA2/TimelineWidget.cc
@@ -7,18 +7,27 @@ class ItemDelegate : public QStyledItemDelegate {
 public:
     ItemDelegate(QObject *parent = 0) : QStyledItemDelegate(parent) {
     }
-    void paint(QPainter * painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
-        QStyledItemDelegate::paint(painter, option, index);
-    }
-    QSize sizeHint(const QStyleOptionViewItem & /* option */, const QModelIndex & /* index */) const {
-        return QSize(5, 5);
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex & /* index */) const {
+        const QRect &rect = option.rect;
+        int offset = rect.y() + ((rect.height() - 10) / 2);
+        painter->restore();
+        painter->setRenderHint(QPainter::Antialiasing);
+        painter->setPen(Qt::NoPen);
+        painter->setBrush(option.palette.foreground());
+        painter->drawEllipse(rect.x() + 5, offset, 10, 10);
+        painter->save();
     }
 };
 
 class TableModel : public QAbstractTableModel {
 public:
-    TableModel(vpvl::PMDModel *model, QObject *parent = 0)
-        : QAbstractTableModel(parent), m_model(model) {
+    TableModel(QObject *parent = 0)
+        : QAbstractTableModel(parent), m_model(0) {
+        setModel(0);
+    }
+
+    void setModel(vpvl::PMDModel *model) {
+        m_bones.clear();
         if (model) {
             vpvl::BoneList bones = model->bones();
             uint32_t nBones = bones.size();
@@ -31,13 +40,14 @@ public:
         else {
             m_bones.append(tr("Camera"));
         }
+        reset();
     }
 
     int rowCount(const QModelIndex & /* parent */) const {
         return m_bones.size();
     }
     int columnCount(const QModelIndex & /* parent */) const {
-        return 100;
+        return 18000;
     }
     QVariant data(const QModelIndex &index, int role) const {
         if (!index.isValid() || role != Qt::DisplayRole)
@@ -45,8 +55,12 @@ public:
         return QVariant();
     }
     QVariant headerData(int section, Qt::Orientation orientation, int role) const {
-        if (orientation == Qt::Vertical && role != Qt::SizeHintRole)
+        if (role != Qt::DisplayRole)
+            return QVariant();
+        if (orientation == Qt::Vertical)
             return m_bones.at(section);
+        else if (orientation == Qt::Horizontal)
+            return section + 1;
         return QVariant();
     }
 
@@ -61,30 +75,29 @@ TimelineWidget::TimelineWidget(QWidget *parent) :
     m_tableModel(0),
     m_selectedModel(0)
 {
-    m_tableModel = new TableModel(0, this);
+    m_tableModel = new TableModel();
     m_tableView = new QTableView();
     m_tableView->setShowGrid(true);
-    m_tableView->horizontalHeader()->hide();
     m_tableView->setModel(m_tableModel);
     m_tableView->setSelectionBehavior(QAbstractItemView::SelectItems);
-    ItemDelegate *delegate = new ItemDelegate(this);
-    m_tableView->setItemDelegate(delegate);
+    //ItemDelegate *delegate = new ItemDelegate(this);
+    //m_tableView->setItemDelegate(delegate);
+    m_tableView->setSelectionMode(QAbstractItemView::SingleSelection);
     QVBoxLayout *layout = new QVBoxLayout();
     layout->addWidget(m_tableView);
     layout->setContentsMargins(0, 0, 0, 0);
+    setMinimumSize(260, 480);
     setLayout(layout);
-    setMinimumSize(640, 480);
 }
 
 TimelineWidget::~TimelineWidget()
 {
     delete m_tableModel;
+    delete m_tableView;
 }
 
 void TimelineWidget::setModel(vpvl::PMDModel *model)
 {
     m_selectedModel = model;
-    delete m_tableModel;
-    m_tableModel = new TableModel(model);
-    m_tableView->setModel(m_tableModel);
+    m_tableModel->setModel(model);
 }
