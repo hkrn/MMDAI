0d63f772322037b3b5d8f5741643ab9cab66ede4
hkrn <hikarin.jp@gmail.com>
2011-07-13 00:53:47 +0900

    classify

--- libvpvl/render/common/main.cc
+++ libvpvl/render/common/main.cc
@@ -155,63 +155,6 @@ private:
 
 }
 
-static bool InitializeSurface(SDL_Surface *&surface, int width, int height)
-{
-    SDL_WM_SetCaption("libvpvl render testing program", NULL);
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
-    const SDL_VideoInfo *info = SDL_GetVideoInfo();
-    if (!info) {
-        fprintf(stderr, "Unable to get video info: %s\n", SDL_GetError());
-        return false;
-    }
-    int bpp = info->vfmt->BitsPerPixel;
-    if (SDL_VideoModeOK(width, height, bpp, SDL_OPENGL)) {
-        if ((surface = SDL_SetVideoMode(width, height, bpp, SDL_OPENGL)) == NULL) {
-            fprintf(stderr, "Unable to init surface: %s\n", SDL_GetError());
-            return false;
-        }
-    }
-    else {
-        fprintf(stderr, "It seems OpenGL is not supported\n");
-        return false;
-    }
-    return true;
-}
-
-static void DrawSurface(vpvl::gl::Renderer *renderer)
-{
-    renderer->initializeSurface();
-    renderer->drawSurface();
-    SDL_GL_SwapBuffers();
-}
-
-static bool PollEvents()
-{
-    SDL_Event event;
-    SDLKey key;
-    while (SDL_PollEvent(&event)) {
-        switch (event.type) {
-        case SDL_QUIT:
-            return true;
-        case SDL_KEYDOWN:
-            key = event.key.keysym.sym;
-            if (key == SDLK_ESCAPE || key == SDLK_q)
-                return true;
-            break;
-        default:
-            break;
-        }
-    }
-    return false;
-}
-
 static Uint32 UpdateTimer(Uint32 internal, void *data)
 {
     vpvl::gl::Renderer *renderer = static_cast<vpvl::gl::Renderer *>(data);
@@ -222,120 +165,202 @@ static Uint32 UpdateTimer(Uint32 internal, void *data)
     return internal;
 }
 
+class UI
+{
+public:
+    UI(int argc, char **argv)
+        : m_surface(0),
+          m_world(0),
+          m_dispatcher(&m_config),
+          m_broadphase(btVector3(-400.0f, -400.0f, -400.0f), btVector3(400.0f, 400.0f, 400.0f), 1024),
+          m_delegate(internal::kSystemDir),
+          m_renderer(&m_delegate, internal::kWidth, internal::kHeight, internal::kFPS),
+          m_model(0),
+          m_modelData(0),
+          m_motionData(0),
+          m_cameraData(0),
+          m_stageData(0),
+          m_argc(argc),
+          m_argv(argv)
+    {
+        m_world = new btDiscreteDynamicsWorld(&m_dispatcher, &m_broadphase, &m_solver, &m_config);
+        m_world->setGravity(btVector3(0.0f, -9.8f * 2.0f, 0.0f));
+        m_world->getSolverInfo().m_numIterations = static_cast<int>(10.0f * 60.0f / internal::kFPS);
+    }
+    ~UI() {
+        m_renderer.unloadModel(m_model);
+#if VPVL_SDL_LOAD_ASSET
+        // m_renderer.unloadAsset(&m_stage);
+#endif
+        delete m_model;
+        delete m_world;
+        delete[] m_modelData;
+        delete[] m_motionData;
+        delete[] m_cameraData;
+        delete[] m_stageData;
+        SDL_FreeSurface(m_surface);
+        m_surface = 0;
+    }
+
+    bool initialize() {
+        if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) < 0) {
+            fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
+            return false;
+        }
+        atexit(SDL_Quit);
+        if (IMG_Init(IMG_INIT_JPG|IMG_INIT_PNG) < 0) {
+            fprintf(stderr, "Unable to init SDL_image: %s\n", IMG_GetError());
+            return false;
+        }
+        atexit(IMG_Quit);
+
+        if (!initializeSurface(internal::kWidth, internal::kHeight))
+            return false;
+
+        GLenum err;
+        if (!vpvl::gl::Renderer::initializeGLEW(err)) {
+            fprintf(stderr, "Unable to init GLEW: %s\n", glewGetErrorString(err));
+            return false;
+        }
+
+        if (!loadScene())
+            return false;
+
+        return true;
+    }
+    int execute() {
+        uint32_t interval = static_cast<uint32_t>(1000.0f / internal::kFPS);
+        SDL_TimerID timerID = SDL_AddTimer(interval, UpdateTimer, &m_renderer);
+        while (true) {
+            if (pollEvent())
+                return 0;
+            draw();
+            SDL_GL_SwapBuffers();
+        }
+        SDL_RemoveTimer(timerID);
+        sleep(1); /* wait for ensuring SDL's timer thread is killed */
+        return 0;
+    }
+
+protected:
+    virtual void draw() {
+        m_renderer.initializeSurface();
+        m_renderer.drawSurface();
+    }
+
+private:
+    bool initializeSurface(int width, int height) {
+        SDL_WM_SetCaption("libvpvl + SDL", NULL);
+        SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+        SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+        SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+        const SDL_VideoInfo *info = SDL_GetVideoInfo();
+        if (!info) {
+            fprintf(stderr, "Unable to get video info: %s\n", SDL_GetError());
+            return false;
+        }
+        int bpp = info->vfmt->BitsPerPixel;
+        if (SDL_VideoModeOK(width, height, bpp, SDL_OPENGL)) {
+            if ((m_surface = SDL_SetVideoMode(width, height, bpp, SDL_OPENGL)) == NULL) {
+                fprintf(stderr, "Unable to init surface: %s\n", SDL_GetError());
+                return false;
+            }
+        }
+        else {
+            fprintf(stderr, "It seems OpenGL is not supported\n");
+            return false;
+        }
+        return true;
+    }
+    bool loadScene() {
+        size_t size = 0;
+        internal::slurpFile(internal::concatPath(internal::kModelDir, internal::kModelName), m_modelData, size);
+        m_model = new vpvl::PMDModel();
+        if (!m_model->load(m_modelData, size)) {
+            fprintf(stderr, "Failed parsing the model\n");
+            return false;
+        }
+        m_renderer.loadModel(m_model, internal::kModelDir);
+
+#if VPVL_SDL_LOAD_ASSET
+        internal::slurpFile(internal::concatPath(internal::kStageDir, internal::kStageName), m_stageData, size);
+        if (!m_stage.load(m_stageData, size)) {
+            fprintf(stderr, "Failed parsing the stage\n");
+            return -1;
+        }
+        m_renderer.loadAsset(&m_stage, internal::kStageDir);
+#endif
+
+        vpvl::Scene *scene = m_renderer.scene();
+        m_renderer.setLighting();
+        scene->addModel(m_model);
+
+        internal::slurpFile(internal::kMotion, m_motionData, size);
+        if (!m_motion.load(m_motionData, size)) {
+            fprintf(stderr, "Failed parsing the model motion\n");
+            return false;
+        }
+        m_model->addMotion(&m_motion);
+
+        internal::slurpFile(internal::kCamera, m_cameraData, size);
+        if (!m_camera.load(m_cameraData, size)) {
+            fprintf(stderr, "Failed parsing the camera motion\n");
+            return false;
+        }
+        //scene.setCamera(btVector3(0.0f, 50.0f, 0.0f), btVector3(0.0f, 0.0f, 0.0f), 60.0f, 50.0f);
+        scene->setCameraMotion(&m_camera);
+        scene->setWorld(m_world);
+
+        return true;
+    }
+    bool pollEvent() {
+        SDL_Event event;
+        SDLKey key;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                return true;
+            case SDL_KEYDOWN:
+                key = event.key.keysym.sym;
+                if (key == SDLK_ESCAPE || key == SDLK_q)
+                    return true;
+                break;
+            default:
+                break;
+            }
+        }
+        return false;
+    }
+
+    SDL_Surface *m_surface;
+    btDiscreteDynamicsWorld *m_world;
+    btDefaultCollisionConfiguration m_config;
+    btCollisionDispatcher m_dispatcher;
+    btAxisSweep3 m_broadphase;
+    btSequentialImpulseConstraintSolver m_solver;
+    internal::Delegate m_delegate;
+    vpvl::gl::Renderer m_renderer;
+    vpvl::PMDModel *m_model; /* for destruction order problem with btDiscreteDynamicsWorld */
+    vpvl::VMDMotion m_motion;
+    vpvl::VMDMotion m_camera;
+    vpvl::XModel m_stage;
+    uint8_t *m_modelData;
+    uint8_t *m_motionData;
+    uint8_t *m_cameraData;
+    uint8_t *m_stageData;
+    int m_argc;
+    char **m_argv;
+};
+
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    (void) argv;
-
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) < 0) {
-        fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
+    UI ui(argc, argv);
+    if (!ui.initialize())
         return -1;
-    }
-    atexit(SDL_Quit);
-    if (IMG_Init(IMG_INIT_JPG|IMG_INIT_PNG) < 0) {
-        fprintf(stderr, "Unable to init SDL_image: %s\n", IMG_GetError());
-        return -1;
-    }
-    atexit(IMG_Quit);
-
-    SDL_Surface *surface;
-    if (!InitializeSurface(surface, internal::kWidth, internal::kHeight)) {
-        return -1;
-    }
-
-    GLenum err;
-    if (!vpvl::gl::Renderer::initializeGLEW(err)) {
-        fprintf(stderr, "Unable to init GLEW: %s\n", glewGetErrorString(err));
-        return -1;
-    }
-
-    const float dist = 400.0f;
-    btDefaultCollisionConfiguration config;
-    btCollisionDispatcher dispatcher(&config);
-    btAxisSweep3 broadphase(btVector3(-dist, -dist, -dist), btVector3(dist, dist, dist), 1024);
-    btSequentialImpulseConstraintSolver solver;
-    btDiscreteDynamicsWorld world(&dispatcher, &broadphase, &solver, &config);
-    world.setGravity(btVector3(0.0f, -9.8f * 2.0f, 0.0f));
-    world.getSolverInfo().m_numIterations = static_cast<int>(10.0f * 60.0f / internal::kFPS);
-
-    internal::Delegate delegate(internal::kSystemDir);
-    vpvl::gl::Renderer renderer(&delegate, internal::kWidth, internal::kHeight, internal::kFPS);
-
-    uint8_t *modelData = 0;
-    uint8_t *motionData = 0;
-    uint8_t *cameraData = 0;
-    uint8_t *stageData = 0;
-    size_t size = 0;
-
-    internal::slurpFile(internal::concatPath(internal::kModelDir, internal::kModelName), modelData, size);
-    vpvl::PMDModel model;
-    if (!model.load(modelData, size)) {
-        fprintf(stderr, "Failed parsing the model\n");
-        delete[] modelData;
-        return -1;
-    }
-    renderer.loadModel(&model, internal::kModelDir);
-
-#if VPVL_SDL_LOAD_ASSET
-    internal::slurpFile(internal::concatPath(internal::kStageDir, internal::kStageName), stageData, size);
-    vpvl::XModel stage;
-    if (!stage.load(stageData, size)) {
-        fprintf(stderr, "Failed parsing the stage\n");
-        delete[] modelData;
-        delete[] stageData;
-        return -1;
-    }
-    renderer.loadAsset(&stage, internal::kStageDir);
-#endif
-
-    vpvl::Scene *scene = renderer.scene();
-    renderer.setLighting();
-    scene->addModel(&model);
-    scene->setWorld(&world);
-
-    internal::slurpFile(internal::kMotion, motionData, size);
-    vpvl::VMDMotion motion;
-    if (!motion.load(motionData, size)) {
-        fprintf(stderr, "Failed parsing the model motion\n");
-        delete[] modelData;
-        delete[] stageData;
-        delete[] motionData;
-        return -1;
-    }
-    model.addMotion(&motion);
-
-    internal::slurpFile(internal::kCamera, cameraData, size);
-    vpvl::VMDMotion camera;
-    if (!camera.load(cameraData, size)) {
-        fprintf(stderr, "Failed parsing the camera motion\n");
-        delete[] modelData;
-        delete[] stageData;
-        delete[] motionData;
-        delete[] cameraData;
-        return -1;
-    }
-    //scene.setCamera(btVector3(0.0f, 50.0f, 0.0f), btVector3(0.0f, 0.0f, 0.0f), 60.0f, 50.0f);
-    scene->setCameraMotion(&camera);
-
-    uint32_t interval = static_cast<uint32_t>(1000.0f / internal::kFPS);
-    SDL_TimerID timerID = SDL_AddTimer(interval, UpdateTimer, &renderer);
-    while (true) {
-        if (PollEvents())
-            break;
-        DrawSurface(&renderer);
-    }
-    SDL_RemoveTimer(timerID);
-
-    renderer.unloadModel(&model);
-#if VPVL_SDL_LOAD_ASSET
-    //renderer.unloadAsset(&stage);
-#endif
-    SDL_FreeSurface(surface);
-
-    delete[] stageData;
-    delete[] motionData;
-    delete[] modelData;
-    delete[] cameraData;
-
-    return 0;
+    return ui.execute();
 }
