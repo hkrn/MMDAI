d5ce8fa70de05ed0a25e02175220ceac21adbad0
hkrn <hikarin.jp@gmail.com>
2011-07-18 22:58:15 +0900

    added parseMotion and parseCamera

--- libvpvl/qtest/motion/TestVMDMotion.cc
+++ libvpvl/qtest/motion/TestVMDMotion.cc
@@ -12,6 +12,8 @@ public:
 
 private Q_SLOTS:
     void parseEmpty();
+    void parseMotion();
+    void parseCamera();
 };
 
 TestVMDMotion::TestVMDMotion()
@@ -24,6 +26,48 @@ void TestVMDMotion::parseEmpty()
     vpvl::VMDMotionDataInfo info;
     QVERIFY(!motion.preparse(reinterpret_cast<const uint8_t *>(""), 0, info));
     QCOMPARE(motion.error(), vpvl::VMDMotion::kInvalidHeaderError);
+}
+
+void TestVMDMotion::parseMotion()
+{
+    QFile file("../../../gtest/res/motion.vmd");
+    if (file.open(QFile::ReadOnly)) {
+        QByteArray bytes = file.readAll();
+        const uint8_t *data = reinterpret_cast<const uint8_t *>(bytes.constData());
+        size_t size = bytes.size();
+        vpvl::VMDMotion motion;
+        vpvl::VMDMotionDataInfo result;
+        QVERIFY(motion.preparse(data, size, result));
+        QVERIFY(motion.load(data, size));
+        QCOMPARE(result.boneKeyFrameCount, size_t(motion.bone().frames().size()));
+        QCOMPARE(result.cameraKeyFrameCount, size_t(motion.camera().frames().size()));
+        QCOMPARE(result.faceKeyFrameCount, size_t(motion.face().frames().size()));
+        QCOMPARE(motion.error(), vpvl::VMDMotion::kNoError);
+    }
+    else {
+        QSKIP("Require a motion to test this", SkipSingle);
+    }
+}
+
+void TestVMDMotion::parseCamera()
+{
+    QFile file("../../../gtest/res/camera.vmd");
+    if (file.open(QFile::ReadOnly)) {
+        QByteArray bytes = file.readAll();
+        const uint8_t *data = reinterpret_cast<const uint8_t *>(bytes.constData());
+        size_t size = bytes.size();
+        vpvl::VMDMotion motion;
+        vpvl::VMDMotionDataInfo result;
+        QVERIFY(motion.preparse(data, size, result));
+        QVERIFY(motion.load(data, size));
+        QCOMPARE(result.boneKeyFrameCount, size_t(motion.bone().frames().size()));
+        QCOMPARE(result.cameraKeyFrameCount, size_t(motion.camera().frames().size()));
+        QCOMPARE(result.faceKeyFrameCount, size_t(motion.face().frames().size()));
+        QCOMPARE(motion.error(), vpvl::VMDMotion::kNoError);
+    }
+    else {
+        QSKIP("Require a motion to test this", SkipSingle);
+    }
 }
 
 QTEST_APPLESS_MAIN(TestVMDMotion)
