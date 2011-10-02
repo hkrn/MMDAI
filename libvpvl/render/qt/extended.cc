b5c32eb4763bac6585c932e9f450444c03b02483
hkrn <hikarin.jp@gmail.com>
2011-10-03 01:06:25 +0900

    code for executing valgrind

--- libvpvl/render/qt/extended.cc
+++ libvpvl/render/qt/extended.cc
@@ -3,7 +3,14 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+#if 1
     UI ui; ui.show();
     return app.exec();
+#else
+    UI *ui = new UI();
+    ui->show();
+    delete ui;
+    return 0;
+#endif
 }
 
