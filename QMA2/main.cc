e350592a1ce45131941e4124a8c8555887e45c9c
hkrn <hikarin.jp@gmail.com>
2011-10-30 22:21:04 +0900

    MMDAI2 0.6.0 -> 0.7.0

--- QMA2/main.cc
+++ QMA2/main.cc
@@ -116,7 +116,7 @@ int main(int argc, char *argv[])
     Application a(argc, argv);
     QList<QTranslatorPtr> translators;
     a.setApplicationName("MMDAI2");
-    a.setApplicationVersion("0.6.0");
+    a.setApplicationVersion("0.7.0");
     a.setOrganizationDomain("mmdai.github.com");
     a.setOrganizationName("MMDAI");
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
