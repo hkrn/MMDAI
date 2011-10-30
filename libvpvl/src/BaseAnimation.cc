hkrn/MMDAI

commit c9fceb23314ca03665525834847642f6b59673f5

should retain currentFrame value at seek, not advance

libvpvl/src/BaseAnimation.cc
@@ -76,9 +76,7 @@ BaseAnimation::~BaseAnimation()
 
 void BaseAnimation::advance(float deltaFrame)
 {
-    seek(m_currentFrame);
-    m_previousFrame = m_currentFrame;
-    m_currentFrame += deltaFrame;
+    seek(m_currentFrame + deltaFrame);
 }
 
 void BaseAnimation::rewind(float target, float deltaFrame)

libvpvl/src/BoneAnimation.cc
@@ -131,6 +131,8 @@ void BoneAnimation::seek(float frameAt)
             bone->setRotation(bone->rotation().slerp(node->rotation, m_blendRate));
         }
     }
+    m_previousFrame = m_currentFrame;
+    m_currentFrame = frameAt;
 }
 
 void BoneAnimation::takeSnap(const Vector3 &center)

libvpvl/src/FaceAnimation.cc
@@ -98,6 +98,8 @@ void FaceAnimation::seek(float frameAt)
         else
             face->setWeight(face->weight() * (1.0f - m_blendRate) + node->weight * m_blendRate);
     }
+    m_previousFrame = m_currentFrame;
+    m_currentFrame = frameAt;
 }
 
 void FaceAnimation::takeSnap(const Vector3 & /* center */)

commit 76d7537b7942de3bdc491bd59492078aeb27a81e

fixed a bug caused by c9fceb23314ca03665525834847642f6b59673f5

libvpvl/src/BaseAnimation.cc
@@ -76,7 +76,8 @@ BaseAnimation::~BaseAnimation()
 
 void BaseAnimation::advance(float deltaFrame)
 {
-    seek(m_currentFrame + deltaFrame);
+    seek(m_currentFrame);
+    m_currentFrame += deltaFrame;
 }
 
 void BaseAnimation::rewind(float target, float deltaFrame)

libvpvl/src/CameraAnimation.cc
@@ -200,6 +200,8 @@ void CameraAnimation::seek(float frameAt)
         m_angle = angleFrom;
         m_fovy = fovyFrom;
     }
+    m_previousFrame = m_currentFrame;
+    m_currentFrame = frameAt;
 }
 
 void CameraAnimation::takeSnap(const Vector3 & /* center */)