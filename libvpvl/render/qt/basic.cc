d4354ca7f6156cd6bdcf1cf9868053f4c6f4eb2c
hkrn <hikarin.jp@gmail.com>
2011-11-12 03:59:29 +0900

    added valgrind check mode

--- libvpvl/render/qt/basic.cc
+++ libvpvl/render/qt/basic.cc
@@ -3,6 +3,13 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+#if 0
     UI ui; ui.show();
     return app.exec();
+#else
+    UI *ui = new UI();
+    ui->show();
+    delete ui;
+    return 0;
+#endif
 }
