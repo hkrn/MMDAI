ef0c58a7d0357e7e73012e280b82248bf42a394e
hkrn <hikarin.jp@gmail.com>
2011-11-26 16:52:39 +0900

    optimized VBO usage of Grid class

--- QMA2/Grid.h
+++ QMA2/Grid.h
@@ -38,29 +38,26 @@
 #define GRID_H
 
 #include <QtGlobal>
+#include <QtOpenGL/QtOpenGL>
 
 #include <vpvl/Scene.h>
-#include <LinearMath/btAlignedObjectArray.h>
-#include <LinearMath/btVector3.h>
 
 namespace internal {
 
 class Grid {
 public:
+    struct Vertex {
+        vpvl::Vector3 position;
+        vpvl::Vector3 color;
+    };
+
     static const int kLimit = 50;
 
-    Grid() : m_vbo(0), m_cbo(0), m_ibo(0), m_list(0), m_enabled(true) {}
+    Grid() : m_vbo(0), m_ibo(0), m_list(0), m_enabled(true) {}
     ~Grid() {
-        m_vertices.clear();
-        m_colors.clear();
-        m_indices.clear();
         if (m_vbo) {
             glDeleteBuffers(1, &m_vbo);
             m_vbo = 0;
-        }
-        if (m_cbo) {
-            glDeleteBuffers(1, &m_cbo);
-            m_cbo = 0;
         }
         if (m_ibo) {
             glDeleteBuffers(1, &m_ibo);
@@ -74,44 +71,43 @@ public:
 
     void initialize() {
         // draw black grid
-        btVector3 lineColor(0.5f, 0.5f, 0.5f);
-        uint16_t index = 0;
+        static const vpvl::Vector3 zero(0.0f, 0.0f, 0.0f);
+        static const vpvl::Vector3 lineColor(0.5f, 0.5f, 0.5f);
+        uint8_t index = 0;
         for (int x = -kLimit; x <= kLimit; x += 5)
-            addLine(btVector3(x, 0.0, -kLimit), btVector3(x, 0.0, x == 0 ? 0.0 : kLimit), lineColor, index);
+            addLine(vpvl::Vector3(x, 0.0, -kLimit), vpvl::Vector3(x, 0.0, x == 0 ? 0.0 : kLimit), lineColor, index);
         for (int z = -kLimit; z <= kLimit; z += 5)
-            addLine(btVector3(-kLimit, 0.0f, z), btVector3(z == 0 ? 0.0f : kLimit, 0.0f, z), lineColor, index);
+            addLine(vpvl::Vector3(-kLimit, 0.0f, z), vpvl::Vector3(z == 0 ? 0.0f : kLimit, 0.0f, z), lineColor, index);
         // X coordinate (red)
-        addLine(btVector3(0.0f, 0.0f, 0.0f), btVector3(kLimit, 0.0f, 0.0f), btVector3(1.0f, 0.0f, 0.0f), index);
+        addLine(zero, vpvl::Vector3(kLimit, 0.0f, 0.0f), vpvl::Vector3(1.0f, 0.0f, 0.0f), index);
         // Y coordinate (green)
-        addLine(btVector3(0.0f, 0.0f, 0.0f), btVector3(0.0f, kLimit, 0.0f), btVector3(0.0f, 1.0f, 0.0f), index);
+        addLine(zero, vpvl::Vector3(0.0f, kLimit, 0.0f), vpvl::Vector3(0.0f, 1.0f, 0.0f), index);
         // Z coordinate (blue)
-        addLine(btVector3(0.0f, 0.0f, 0.0f), btVector3(0.0f, 0.0f, kLimit), btVector3(0.0f, 0.0f, 1.0f), index);
+        addLine(zero, vpvl::Vector3(0.0f, 0.0f, kLimit), vpvl::Vector3(0.0f, 0.0f, 1.0f), index);
         m_list = glGenLists(1);
         glGenBuffers(1, &m_vbo);
         glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-        glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(btVector3), &m_vertices[0], GL_STATIC_DRAW);
-        glGenBuffers(1, &m_cbo);
-        glBindBuffer(GL_ARRAY_BUFFER, m_cbo);
-        glBufferData(GL_ARRAY_BUFFER, m_colors.size() * sizeof(btVector3), &m_colors[0], GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, m_vertices.count() * sizeof(Vertex), &m_vertices[0].position, GL_STATIC_DRAW);
         glGenBuffers(1, &m_ibo);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_indices.size() * sizeof(uint16_t), &m_indices[0], GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_indices.count() * sizeof(uint8_t), &m_indices[0], GL_STATIC_DRAW);
         // start compiling to render with list cache
         glNewList(m_list, GL_COMPILE);
         glDisable(GL_LIGHTING);
         glEnableClientState(GL_VERTEX_ARRAY);
         glEnableClientState(GL_COLOR_ARRAY);
         glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-        glVertexPointer(3, GL_FLOAT, sizeof(btVector3), 0);
-        glBindBuffer(GL_ARRAY_BUFFER, m_cbo);
-        glColorPointer(3, GL_FLOAT, sizeof(btVector3), 0);
+        glVertexPointer(3, GL_FLOAT, sizeof(Vertex), reinterpret_cast<GLvoid *>(0));
+        glColorPointer(3, GL_FLOAT, sizeof(Vertex), reinterpret_cast<GLvoid *>(sizeof(vpvl::Vector3)));
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
-        glDrawElements(GL_LINES, m_indices.size(), GL_UNSIGNED_SHORT, 0);
+        glDrawElements(GL_LINES, m_indices.count(), GL_UNSIGNED_BYTE, 0);
         glDisableClientState(GL_VERTEX_ARRAY);
         glDisableClientState(GL_COLOR_ARRAY);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
         glEnable(GL_LIGHTING);
+        glDisableClientState(GL_VERTEX_ARRAY);
+        glDisableClientState(GL_COLOR_ARRAY);
         glEndList();
     }
 
@@ -137,22 +133,21 @@ public:
     }
 
 private:
-    void addLine(const btVector3 &from, const btVector3 &to, const btVector3 &color, uint16_t &index) {
-        m_vertices.push_back(from);
-        m_vertices.push_back(to);
-        m_colors.push_back(color);
-        m_colors.push_back(color);
-        m_indices.push_back(index);
-        index++;
-        m_indices.push_back(index);
-        index++;
+    void addLine(const vpvl::Vector3 &from, const vpvl::Vector3 &to, const vpvl::Vector3 &color, uint8_t &index) {
+        Vertex f, t;
+        f.position = from;
+        f.color = color;
+        t.position = to;
+        t.color = color;
+        m_vertices.add(f);
+        m_vertices.add(t);
+        m_indices.add(index++);
+        m_indices.add(index++);
     }
 
-    btAlignedObjectArray<btVector3> m_vertices;
-    btAlignedObjectArray<btVector3> m_colors;
-    btAlignedObjectArray<uint16_t> m_indices;
+    vpvl::Array<Vertex> m_vertices;
+    vpvl::Array<uint8_t> m_indices;
     GLuint m_vbo;
-    GLuint m_cbo;
     GLuint m_ibo;
     GLuint m_list;
     bool m_enabled;
