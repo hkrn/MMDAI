24d886a3599f47817d0148a02452c4e3eb2ef302
hkrn <hikarin.jp@gmail.com>
2012-01-01 01:00:33 +0900

    s/2010-2011/2010-2012/gxms

--- libvpvl/include/vpvl/PMDModel.h
+++ libvpvl/include/vpvl/PMDModel.h
@@ -2,7 +2,7 @@
 /*                                                                   */
 /*  Copyright (c) 2009-2011  Nagoya Institute of Technology          */
 /*                           Department of Computer Science          */
-/*                2010-2011  hkrn                                    */
+/*                2010-2012  hkrn                                    */
 /*                                                                   */
 /* All rights reserved.                                              */
 /*                                                                   */
