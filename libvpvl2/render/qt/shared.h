2fa6d92e099cf7b0fec767364b20f98d5c0d4ae8
hkrn <hikarin.jp@gmail.com>
2012-04-14 02:58:10 +0900

    forgot to enable GL_CULL_FACE and GL_BLEND in renderer

--- libvpvl2/render/qt/shared.h
+++ libvpvl2/render/qt/shared.h
@@ -521,6 +521,11 @@ protected:
         if (!loadScene())
             qFatal("Unable to load scene");
 
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
         resize(kWidth, kHeight);
         startTimer(1000.0f / 60.0f);
         m_timer.start();
