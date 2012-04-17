7babe23a2d4939a25136fdea99c8039cc846c7a2
hkrn <hikarin.jp@gmail.com>
2012-04-18 01:33:09 +0900

    QMA2 => 0.21.2

--- QMA2/main.cc
+++ QMA2/main.cc
@@ -124,7 +124,7 @@ int main(int argc, char *argv[])
     QWidget fake;
     QList<QTranslatorPtr> translators;
     a.setApplicationName("MMDAI2");
-    a.setApplicationVersion("0.21.1");
+    a.setApplicationVersion("0.21.2");
     a.setOrganizationDomain("mmdai.github.com");
     a.setOrganizationName("MMDAI");
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
