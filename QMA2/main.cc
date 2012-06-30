53e220294dd0c63f8e4fd5c15f37b3eb095657be
hkrn <hikarin.jp@gmail.com>
2012-07-01 01:30:52 +0900

    libvpvl => 0.5.0 / QMA2 => 0.24.0

--- QMA2/main.cc
+++ QMA2/main.cc
@@ -126,7 +126,7 @@ int main(int argc, char *argv[])
     QWidget fake;
     QList<QTranslatorPtr> translators;
     a.setApplicationName("MMDAI2");
-    a.setApplicationVersion("0.23.2");
+    a.setApplicationVersion("0.24.0");
     a.setOrganizationDomain("mmdai.github.com");
     a.setOrganizationName("MMDAI");
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
