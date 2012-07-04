2dc156db836dd7b6a0ca713ae9c22076c42e7085
hkrn <hikarin.jp@gmail.com>
2012-07-04 21:33:04 +0900

    make x86_64 binary deployable

--- scripts/linux_deploy.sh
+++ scripts/linux_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-arch=`arch`
+arch=`arch | tr '_' '-'`
 working_directory=`pwd`
 current_directory=`basename $working_directory`
 if [ $current_directory = "QMA1-release-desktop" ]; then
@@ -18,14 +18,22 @@ package_name="${app_name}-linux-${arch}"
 rm -rf ${app_name}.zip ${package_name}
 make
 make install
+strip ${app_name}
+cp ${INSTALL_ROOT}/lib/libassimp.so ${INSTALL_ROOT}/lib/libassimp.so.2
 mkdir ${package_name}
 rm -rf lib
 mkdir lib
-ldd ${app_name} | grep libassimp.so | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../assimp/lib/% lib
+mv -f ../assimp/release/lib/libassimp.so ../assimp/release/lib/libassimp.so.2
+ldd ${app_name} | grep libassimp.so.2 | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../assimp/release/lib/% lib
 ldd ${app_name} | grep libvpvl2.so | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../libvpvl2/release/lib/% lib
 ldd lib/libvpvl2.so.* | grep libvpvl.so | perl -ne 'print [split(/\s+/, $_)]->[3], "\n"' | xargs -i% cp % lib
 ldd ${app_name} | grep libBullet | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../bullet/release/lib/% lib
 ldd ${app_name} | grep libLinearMath.so | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../bullet/release/lib/% lib
+ldd ${app_name} | grep libavcodec.so | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../libav/libav_release/lib/% lib
+ldd ${app_name} | grep libavformat.so | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../libav/libav_release/lib/% lib
+ldd ${app_name} | grep libavutil.so | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../libav/libav_release/lib/% lib
+ldd ${app_name} | grep libswscale.so | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../libav/libav_release/lib/% lib
+ldd ${app_name} | grep libportaudio.so | perl -ne 'print [split(/\s+/, $_)]->[1], "\n"' | xargs -i% cp ../portaudio/build/scons/posix/% lib
 ldd ${app_name} | grep libQt | perl -ne 'print [split(/\s+/, $_)]->[3], "\n"' | xargs -i% cp % lib
 strip lib/*
 rm -rf plugins
@@ -41,7 +49,7 @@ rm -rf phonon_backend
 rm -rf script
 rm -rf webkit
 cd ..
-mv ${app_name} lib locales plugins resources ${package_name}
+mv ${app_name} lib locales plugins ${package_name}
 find ${package_name} -exec touch -t `date +%Y%m%d0000` {} \;
 zip -r ${app_name}.zip ${package_name}
 
