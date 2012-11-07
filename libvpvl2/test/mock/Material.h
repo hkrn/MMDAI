2982afbdab6aaf927702b75f89d51f0d6042a1be
hkrn <hikarin.jp@gmail.com>
2012-11-08 01:16:51 +0900

    added missing mock interfaces

---
+++ libvpvl2/test/mock/Material.h
@@ -0,0 +1,99 @@
+namespace vpvl2 {
+
+class MockIMaterial : public IMaterial {
+ public:
+  MOCK_CONST_METHOD0(parentModelRef,
+      IModel*());
+  MOCK_CONST_METHOD0(name,
+      const IString*());
+  MOCK_CONST_METHOD0(englishName,
+      const IString*());
+  MOCK_CONST_METHOD0(userDataArea,
+      const IString*());
+  MOCK_CONST_METHOD0(mainTexture,
+      const IString*());
+  MOCK_CONST_METHOD0(sphereTexture,
+      const IString*());
+  MOCK_CONST_METHOD0(toonTexture,
+      const IString*());
+  MOCK_CONST_METHOD0(sphereTextureRenderMode,
+      SphereTextureRenderMode());
+  MOCK_CONST_METHOD0(ambient,
+      const Color&());
+  MOCK_CONST_METHOD0(diffuse,
+      const Color&());
+  MOCK_CONST_METHOD0(specular,
+      const Color&());
+  MOCK_CONST_METHOD0(edgeColor,
+      const Color&());
+  MOCK_CONST_METHOD0(mainTextureBlend,
+      const Color&());
+  MOCK_CONST_METHOD0(sphereTextureBlend,
+      const Color&());
+  MOCK_CONST_METHOD0(toonTextureBlend,
+      const Color&());
+  MOCK_CONST_METHOD0(shininess,
+      float());
+  MOCK_CONST_METHOD0(edgeSize,
+      float());
+  MOCK_CONST_METHOD0(index,
+      int());
+  MOCK_CONST_METHOD0(textureIndex,
+      int());
+  MOCK_CONST_METHOD0(sphereTextureIndex,
+      int());
+  MOCK_CONST_METHOD0(toonTextureIndex,
+      int());
+  MOCK_CONST_METHOD0(indices,
+      int());
+  MOCK_CONST_METHOD0(isSharedToonTextureUsed,
+      bool());
+  MOCK_CONST_METHOD0(isCullFaceDisabled,
+      bool());
+  MOCK_CONST_METHOD0(hasShadow,
+      bool());
+  MOCK_CONST_METHOD0(isShadowMapDrawn,
+      bool());
+  MOCK_CONST_METHOD0(isSelfShadowDrawn,
+      bool());
+  MOCK_CONST_METHOD0(isEdgeDrawn,
+      bool());
+  MOCK_METHOD1(setName,
+      void(const IString *value));
+  MOCK_METHOD1(setEnglishName,
+      void(const IString *value));
+  MOCK_METHOD1(setUserDataArea,
+      void(const IString *value));
+  MOCK_METHOD1(setMainTexture,
+      void(const IString *value));
+  MOCK_METHOD1(setSphereTexture,
+      void(const IString *value));
+  MOCK_METHOD1(setToonTexture,
+      void(const IString *value));
+  MOCK_METHOD1(setSphereTextureRenderMode,
+      void(SphereTextureRenderMode value));
+  MOCK_METHOD1(setAmbient,
+      void(const Color &value));
+  MOCK_METHOD1(setDiffuse,
+      void(const Color &value));
+  MOCK_METHOD1(setSpecular,
+      void(const Color &value));
+  MOCK_METHOD1(setEdgeColor,
+      void(const Color &value));
+  MOCK_METHOD1(setShininess,
+      void(float value));
+  MOCK_METHOD1(setEdgeSize,
+      void(float value));
+  MOCK_METHOD1(setMainTextureIndex,
+      void(int value));
+  MOCK_METHOD1(setSphereTextureIndex,
+      void(int value));
+  MOCK_METHOD1(setToonTextureIndex,
+      void(int value));
+  MOCK_METHOD1(setIndices,
+      void(int value));
+  MOCK_METHOD1(setFlags,
+      void(int value));
+};
+
+}  // namespace vpvl2
---
+++ libvpvl2/test/mock/Vertex.h
@@ -0,0 +1,49 @@
+namespace vpvl2 {
+
+class MockIVertex : public IVertex {
+ public:
+  MOCK_CONST_METHOD0(parentModelRef,
+      IModel*());
+  MOCK_CONST_METHOD2(performSkinning,
+      void(Vector3 &position, Vector3 &normal));
+  MOCK_METHOD0(reset,
+      void());
+  MOCK_CONST_METHOD0(origin,
+      const Vector3&());
+  MOCK_CONST_METHOD0(normal,
+      const Vector3&());
+  MOCK_CONST_METHOD0(textureCoord,
+      const Vector3&());
+  MOCK_CONST_METHOD1(uv,
+      const Vector4&(int index));
+  MOCK_CONST_METHOD0(delta,
+      const Vector3&());
+  MOCK_CONST_METHOD0(type,
+      Type());
+  MOCK_CONST_METHOD0(edgeSize,
+      float());
+  MOCK_CONST_METHOD1(weight,
+      float(int index));
+  MOCK_CONST_METHOD1(bone,
+      IBone*(int index));
+  MOCK_CONST_METHOD0(index,
+      int());
+  MOCK_METHOD1(setOrigin,
+      void(const Vector3 &value));
+  MOCK_METHOD1(setNormal,
+      void(const Vector3 &value));
+  MOCK_METHOD1(setTextureCoord,
+      void(const Vector3 &value));
+  MOCK_METHOD2(setUV,
+      void(int index, const Vector4 &value));
+  MOCK_METHOD1(setType,
+      void(Type value));
+  MOCK_METHOD1(setEdgeSize,
+      void(float value));
+  MOCK_METHOD2(setWeight,
+      void(int index, float weight));
+  MOCK_METHOD2(setBone,
+      void(int index, IBone *value));
+};
+
+}  // namespace vpvl2
