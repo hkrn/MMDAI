ba53f6867234d756fe2fb66a737c89b12aa1f0e1
hkrn <hikarin.jp@gmail.com>
2012-11-11 21:18:57 +0900

    libvpvl2 => 0.8.0 / VPVM => 0.26.0

--- VPVM/main.cc
+++ VPVM/main.cc
@@ -128,7 +128,7 @@ int main(int argc, char *argv[])
     vpvm::Application a(argc, argv);
     QList<QTranslatorPtr> translators;
     a.setApplicationName("MMDAI2");
-    a.setApplicationVersion("0.25.1");
+    a.setApplicationVersion("0.26.0");
     a.setOrganizationDomain("mmdai.github.com");
     a.setOrganizationName("MMDAI");
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
