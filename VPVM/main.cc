e8dc5e2da2289921b66cb1b26edb1fa8f6ff22c1
hkrn <hikarin.jp@gmail.com>
2012-12-08 23:53:36 +0900

    VPVM => 0.27.2

--- VPVM/main.cc
+++ VPVM/main.cc
@@ -129,7 +129,7 @@ int main(int argc, char *argv[])
     vpvm::LoggerWidget::quietLogMessages();
     QList<QTranslatorPtr> translators;
     a.setApplicationName("MMDAI2");
-    a.setApplicationVersion("0.27.1");
+    a.setApplicationVersion("0.27.2");
     a.setOrganizationDomain("mmdai.github.com");
     a.setOrganizationName("MMDAI");
 #if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
