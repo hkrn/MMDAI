774a26b0d784e5cfd9684d021a4418afd84e9aed
hkrn <hikarin.jp@gmail.com>
2013-03-03 16:40:01 +0900

    fixed a bug that doesn't initialize state when GLEW is disabled

--- libvpvl2/src/core/base/Scene.cc
+++ libvpvl2/src/core/base/Scene.cc
@@ -771,15 +771,15 @@ bool Scene::initialize(void *opaque)
         }
         ok = g_isGLEWInitialized = (err == GLEW_OK);
     }
+#else
+    (void) opaque;
+#endif
     /* register default OpenGL states */
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_BLEND);
     glEnable(GL_DEPTH_TEST);
-#else
-    (void) opaque;
-#endif
     return ok;
 }
 
