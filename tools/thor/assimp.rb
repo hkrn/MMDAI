74858605cdb02512c3e84b05e417bb49e7f78504
hkrn <hikarin.jp@gmail.com>
2013-09-01 15:00:26 +0900

    vpvl2 now uses assimp v3.x and upper

--- tools/thor/assimp.rb
+++ tools/thor/assimp.rb
@@ -1,17 +1,16 @@
 require File.dirname(__FILE__) + '/cmake.rb'
-require File.dirname(__FILE__) + '/svn.rb'
+require File.dirname(__FILE__) + '/git.rb'
 
 module Mmdai
 
 class Assimp < Thor
   include Build::CMake
-  include VCS::SVN
+  include VCS::Git
 
   desc "debug", "build assimp for debug"
   method_options :flag => :boolean
   def debug
     checkout
-    rewrite_cmake_file Regexp.compile("assimp\s+STATIC"), "assimp SHARED"
     invoke_build :debug
   end
 
@@ -19,7 +18,6 @@ class Assimp < Thor
   method_options :flag => :boolean
   def release
     checkout
-    rewrite_cmake_file Regexp.compile("assimp\s+SHARED"), "assimp STATIC"
     invoke_build :release
   end
 
@@ -30,34 +28,25 @@ class Assimp < Thor
 
 protected
   def get_uri
-    "https://assimp.svn.sourceforge.net/svnroot/assimp/tags/2.0"
+    "https://github.com/assimp/assimp.git"
   end
 
   def get_directory_name
     "assimp-src"
   end
 
-  def get_build_options(build_type, extra_options)
-    if ENV.key? "ASSIMP_V3" then
-      return {
-        :assimp_enable_boost_workaround => true,
-        :assimp_build_static_lib => (build_type === :release),
-        :assimp_build_assimp_tools => false,
-        :assimp_build_samples => false,
-        :assimp_build_tests => false
-      }
-    else
-      return {
-        :build_assimp_tools => false,
-        :enable_boost_workaround => true
-      }
-    end
+  def get_tag_name
+    "master"
   end
 
-private
-  def rewrite_cmake_file(from, to)
-    path = "#{checkout_path}/code/CMakeLists.txt"
-    gsub_file path, from, to
+  def get_build_options(build_type, extra_options)
+    return {
+      :assimp_enable_boost_workaround => true,
+      :assimp_build_static_lib => (build_type === :release),
+      :assimp_build_assimp_tools => false,
+      :assimp_build_samples => false,
+      :assimp_build_tests => false
+    }
   end
 
 end
